bf0bb02ccd7576615fe9b6769b374caa99f33e9a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-05-13 21:34:38 -0700

    weekly update

--- R/expectation40Year.R
+++ R/expectation40Year.R
@@ -30,7 +30,7 @@ fill <- function(x, direction) {
   	}
   } else {
   	warning('Bad direction.')
-  }
+  }x
   x
 }
 
@@ -305,7 +305,7 @@ sp.1k.days <- clip.series(sp, n.days=round(2.5*250))
 plot.for.ts(sp.1k.days, long=round(2.5*250))
 # 30%/15% out; 25/13% in;
 f.out <- 0
-f.in <- 0.25
+f.in <- 0
 fracs.out <- c(fracs.out, f.out)
 fracs.in <- c(fracs.in, f.in)
 
