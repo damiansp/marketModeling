dbd85cd1a12a27208c128e80a1e5682347f8de10
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-07-30 09:56:17 -0700

    weekly update

--- R/expectation40Year.R
+++ R/expectation40Year.R
@@ -312,15 +312,15 @@ fracs.in <- c(fracs.in, f.in)
 sp.1yr <- clip.series(sp, n.days=round(1.25*250))
 plot.for.ts(sp.1yr, long=round(1.25*250), proj=F)
 # 20%/10% out;  12/6% in;
-f.out <- 0
+f.out <- 0.1
 f.in <- 0
 fracs.out <- c(fracs.out, f.out)
 fracs.in <- c(fracs.in, f.in)
 
 sp.6mos <- clip.series(sp, n.days=round(0.5*250))
 plot.for.ts(sp.6mos, long=round(0.5*250), proj=F)
-# 5%/2.5% out; 6/3% in; 
-f.out <- 0
+# 10%/5% out; 6/3% in; 
+f.out <- 0.05
 f.in <- 0
 fracs.out <- c(fracs.out, f.out)
 fracs.in <- c(fracs.in, f.in)
