033ee6cbae73af55661748aa2bd1b00fca8a84e2
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-04-15 10:32:27 -0700

    weekly update

--- daily/main.py
+++ daily/main.py
@@ -23,13 +23,13 @@ from transacting import TransactionDeterminer
 
 
 # Daily inputs:
-FID_VALUE = 204406
-ET_VALUE = 147330
-TDAM_VALUE = 10562
-FRAC_IN = 0.6200
-FID_MAX = 0.02  # max weight to give my picks in fid acct
-RSI_VALUE = 96744
-ADEL_VALUE = 98456
+FID_VALUE = 206447
+ET_VALUE = 148417
+TDAM_VALUE = 10641
+FRAC_IN = 0.5400
+FID_MAX = 0.08  # max weight to give my picks in fid acct
+RSI_VALUE = 102838
+ADEL_VALUE = 99380
 
 TODAY = datetime.now().date()
 TOMORROW = TODAY + timedelta(1)
@@ -59,7 +59,7 @@ def main():
     #run_hmm_models()
     #best_stock_by_state.main()
     #current_best_stocks = select_state_based_stocks(20)
-    current_best_stocks = ['AG', 'AXON', 'BB', 'CHUY', 'CSIQ', 'DQ', 'FSLR', 'FSM', 'HIVE', 'HZNP', 'LFMD', 'LVS', 'LYB', 'NOAH', 'SBSW', 'SSTK', 'SUPN', 'TGLS', 'TSLA', 'VTSI']
+    current_best_stocks = ['ADUS', 'AMBA', 'AVGO', 'AXON', 'CROX', 'CSIQ', 'EPAM', 'FRO', 'LFMD', 'MANH', 'MCVT', 'NFLX', 'NOAH', 'PCRX', 'PRTA', 'SSTK', 'SYNA', 'TSLA', 'UI', 'URI']
     transactions = (
         pd.read_csv(TRANSACTIONS).rename(columns={'Unnamed: 0': 'stock'}))
     # save backup
